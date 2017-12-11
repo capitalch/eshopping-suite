04237a464c3116e6578cdbb8f4836e035d454a72
Sudipto Mukherjee <sudiptokush@gmail.com>
2017-12-11 11:56:23 +0530

    UI & categories

--- eshop-test/src/app/categories/categories.component.ts
+++ eshop-test/src/app/categories/categories.component.ts
@@ -1,16 +1,60 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { EshopService } from '../eshop.service';
+import { TreeNode } from 'primeng/primeng';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss'],
+  styleUrls: ['./categories.component.scss',
+  "../../../node_modules/font-awesome/css/font-awesome.min.css",
+  "../../../node_modules/primeng/resources/primeng.min.css",
+  "../../../node_modules/primeng/resources/themes/omega/theme.css"],
   encapsulation: ViewEncapsulation.None
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  categories: any[];
+  parentCategories: any[] = [];
+  subs: any;
+
+  constructor(private eshopService: EshopService) { 
+    this.getData();
+  }
 
   ngOnInit() {
+    this.subs = this.eshopService.filterOn('get:categories:with:count').subscribe(d => {
+      d.error ? console.log(d.error) : (
+        this.categories = d.data,
+        this.processLazy()        
+      );
+    });
+  }
+
+  processLazy() {
+    let items: any[];
+    this.parentCategories = this.categories.filter(x => {
+      x.leaf = x.cat_cnt == 0;
+      return (x.parent_id == null);
+    });
+  }
+
+  getData() {
+    let id = 'get:categories:with:count';
+    this.eshopService.httpPost(id, { id: id, params: {} });
+  }
+
+  loadNode(e) {
+    let item = e.node;
+    let children = this.categories.filter(x => {
+      x.leaf = x.cat_cnt == 0;
+      return (item.id == x.parent_id);
+    });
+    item.children = children;
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
 
 }

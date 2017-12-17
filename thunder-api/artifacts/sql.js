4ea5c7e05423e4d5c00dbd1204044199d383636d
sushant <capitalch@gmail.com>
2017-12-17 20:25:56 +0530

    init

--- thunder-api/artifacts/sql.js
+++ thunder-api/artifacts/sql.js
@@ -60,48 +60,60 @@ let sqls = {
          insert into temp1(id,label,parent_id,cat_cnt,product_cnt) values (-1,'Categories',null,0,0);
      select id,label,parent_id,cat_cnt,product_cnt from temp1 t order by t.id;`
   , 'post:query:categories:product:on:input': `with recursive 
-      cte1 as (
-        SELECT id, label, parent_id
-              FROM category
-                  WHERE 
-                  to_tsvector('english', label) @@ to_tsquery('english', %L)
-              union
-                SELECT id, label, parent_id
-                  from category where id in(
-                    select cat_id from product where to_tsvector('english', name) @@ to_tsquery('english', %L)
-                  ) 
-      ), 
-      cte2 as(
-        select id, label, 
-          CASE
-            WHEN parent_id in(select id from cte1)
-            then parent_id
-            else null::int
-          END as parent_id
-          from cte1
-      ) , 
-      cte3 as(  
-          select id, label, parent_id from cte2 
-          union all
-            select c1.id, c1.label, c1.parent_id
-              from category c1 inner join cte3 c3
-                on c1.parent_id = c3.id
-      ) ,
-      cte4 as (select c1.id, c1.label, c1.parent_id, sum(CASE WHEN c2.id is null then 0 else 1 end) as cat_cnt	
-            from cte3 c1 left outer join cte3 c2 
-                on c1.id = c2.parent_id
-                    group by c1.id, c1.label, c1.parent_id
-                      order by c1.id),
-      cte5 as (select c1.id, c1.label, c1.parent_id, min(c1.cat_cnt) as cat_cnt,
-              sum(CASE WHEN p.id is null then 0 else 1 end) as product_cnt
-                from cte4 c1 left outer join product p   	
-                    on c1.id = p.cat_id
-                        group by c1.id, c1.label, c1.parent_id
-                          order by c1
-                          .id)
-      select c2.id, c2.label || ' (' || 
-              CASE WHEN c2.cat_cnt = 0 then c2.product_cnt else c2.cat_cnt END
-              || ')' as label, c2.parent_id, c2.cat_cnt,c2.product_cnt from cte5 c2;`
+  cte0 as (
+      select id, name as label, cat_id as parent_id
+        from product
+            where to_tsvector('english', name) @@ to_tsquery('english', 'lamb')
+    ),
+    cte1 as (
+      SELECT id, label, parent_id
+            FROM cats
+                WHERE 
+                to_tsvector('english', label) @@ to_tsquery('english', 'lamb')
+        union
+      SELECT id, label, parent_id
+        from cats where id in(
+          select parent_id from cte0
+        ) 
+    ), 
+    cte2 as(
+      select id, label, 
+        CASE
+          WHEN parent_id in(select id from cte1)
+          then parent_id
+          else null::int
+        END as parent_id
+        from cte1
+    ) , 
+    cte3 as(  
+        select id, label, parent_id from cte2 
+        union all
+          select c1.id, c1.label, c1.parent_id
+            from category c1 inner join cte3 c3
+              on c1.parent_id = c3.id
+    ) ,
+    cte4 as (select c1.id, c1.label, c1.parent_id, sum(CASE WHEN c2.id is null then 0 else 1 end) as cat_cnt	
+          from cte3 c1 left outer join cte3 c2 
+              on c1.id = c2.parent_id
+                  group by c1.id, c1.label, c1.parent_id
+                    order by c1.id),
+    cte5 as (select c1.id, c1.label, c1.parent_id, min(c1.cat_cnt) as cat_cnt,
+            sum(CASE WHEN p.id is null then 0 else 1 end) as product_cnt
+              from cte4 c1 left outer join product p   	
+                  on c1.id = p.cat_id
+                      group by c1.id, c1.label, c1.parent_id
+                        order by c1
+                        .id),
+    cte6 as (
+        select 0 as id, 'Any category' as label, null::int as parent_id, 0 as cat_cnt, (select count(0) from cte0) as product_cnt
+    )
+    select
+        id, label, parent_id, cat_cnt, product_cnt from cte6 
+          union
+        select c2.id, c2.label || ' (' || 
+            CASE WHEN c2.cat_cnt = 0 then c2.product_cnt else c2.cat_cnt END
+            || ')' as label, c2.parent_id, c2.cat_cnt,c2.product_cnt from cte5 c2
+            order by id;`
   , 'post:query:categories:product:on:input1': `with cte2 as (
     with cte1 as (
         SELECT id, label, cast(nullif(NULL, '') AS int) as parent_id,

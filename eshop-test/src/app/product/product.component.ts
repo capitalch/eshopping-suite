80b7c084ac0bc0c4c16f40962d6e4f248f8334f4
Sudipto Mukherjee <sudiptokush@gmail.com>
2017-12-18 13:02:17 +0530

    category & product display

--- eshop-test/src/app/product/product.component.ts
+++ eshop-test/src/app/product/product.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { EshopService } from '../eshop.service';
+import * as _ from 'underscore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { PaginationService } from '../pagination.service';
 
 @Component({
   selector: 'app-product',
@@ -11,14 +16,22 @@ export class ProductComponent implements OnInit {
 
   products: any[];
   productSub: any;
+  showProduct = false;
 
-  constructor(private eshopService: EshopService) { }
+  // pager object
+  pager: any = {};
+  // paged items
+  pagedItems: any[];
+
+  constructor(private eshopService: EshopService, private paginationService: PaginationService) { }
 
   ngOnInit() {
     this.productSub = this.eshopService.filterOn('post:query:products:on:category').subscribe(d => {
       d.error ? console.log(d.error) : (
-        this.products = d.data,
-        console.log(this.products)
+        this.products = d.data.data,
+        this.showProduct = true,
+        // initialize to page 1
+        this.setPage(1)
       );
     });
   }
@@ -27,6 +40,18 @@ export class ProductComponent implements OnInit {
   {
     alert("ProductID - "+ id);
   }
+
+  setPage(page: number) {
+    if (page < 1 || page > this.pager.totalPages) {
+        return;
+    }
+
+    // get pager object from service
+    this.pager = this.paginationService.getPager(this.products.length, page);
+
+    // get current page of items
+    this.pagedItems = this.products.slice(this.pager.startIndex, this.pager.endIndex + 1);
+}
 
   ngOnDestroy()
   {

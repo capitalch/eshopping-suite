249bfea27d305c61e8cb69bdabab09eef1c7f531
SushantAgrawal <sagarwal@netwoven.com>
2017-12-20 12:43:16 +0530

    init

--- eshop-test1/src/app/app.module.ts
+++ eshop-test1/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
 import { AppComponent } from './app.component';
 import { DashBoardComponent } from './dash-board/dash-board.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { FurtherDetailsComponent } from './further-details/further-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent, DashBoardComponent
+    AppComponent, DashBoardComponent, ProductComponent, 
+    ProductDetailsComponent, FurtherDetailsComponent
   ],
   imports: [
     BrowserModule,
--- eshop-test1/src/app/app.routes.ts
+++ eshop-test1/src/app/app.routes.ts
@@ -1,17 +1,36 @@
-import {RouterModule, Routes} from '@angular/router';
-import {DashBoardComponent} from './dash-board/dash-board.component';
-export const routes : Routes = [
-    {
-      path: '',
-      redirectTo: 'dashBoard',
-      pathMatch: 'full'
-    },
-    // , {
-    //   path: 'lazy',
-    //   loadChildren: './lazy/lazy.module#LazyModule' 
-    // },
-    {
-      path:'dashBoard',
-      component:DashBoardComponent
-    }
-  ];
+import { RouterModule, Routes } from '@angular/router';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { FurtherDetailsComponent } from './further-details/further-Details.component';
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashBoard',
+    pathMatch: 'full'
+  },
+  // , {
+  //   path: 'lazy',
+  //   loadChildren: './lazy/lazy.module#LazyModule' 
+  // },
+  {
+    path: 'dashBoard',
+    component: DashBoardComponent,
+    children: [
+      {
+        path: 'product',
+        component: ProductComponent
+      },
+      {
+        path: 'productDetails',
+        component: ProductDetailsComponent
+        // ,children: [
+        //   {
+        //     path: 'furtherDetails',
+        //     component:FurtherDetailsComponent
+        //   }
+        // ]
+      }
+    ]
+  }
+];

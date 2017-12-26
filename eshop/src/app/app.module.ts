a42397ff23b37d0661df3e815ac1b752c98bd04d
SushantAgrawal <sagarwal@netwoven.com>
2017-12-26 12:18:22 +0530

    init

--- eshop/src/app/app.module.ts
+++ eshop/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppService } from './service/app.service';
 import { BrokerService } from './service/broker.service';
 import { AppComponent } from './app.component';
 import { EmartModule } from './emart/emart.module';
-import {routes} from './app.routes';
+import { routes } from './app.routes';
 // import { TestComponent } from './test/test.component';
 
 @NgModule({
@@ -14,9 +15,10 @@ import {routes} from './app.routes';
     // ,TestComponent
   ],
   imports: [
-    BrowserModule, RouterModule.forRoot(routes)
+    BrowserModule, RouterModule.forRoot(routes), MDBBootstrapModule.forRoot()
     // , EmartModule
   ],
+  schemas: [NO_ERRORS_SCHEMA],
   providers: [AppService, BrokerService],
   bootstrap: [AppComponent]
 })

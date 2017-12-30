744e255dcd65a5ca20abf6390054f99ae23260b7
SushantAgrawal <sagarwal@netwoven.com>
2017-12-30 12:56:27 +0530

    init

--- eshop/src/app/app.module.ts
+++ eshop/src/app/app.module.ts
@@ -10,12 +10,10 @@ import { BrokerService } from './service/broker.service';
 import { AppComponent } from './app.component';
 import { EmartModule } from './emart/emart.module';
 import { routes } from './app.routes';
-// import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent
-    // ,TestComponent
   ],
   imports: [
     BrowserModule, RouterModule.forRoot(routes), BrowserAnimationsModule,

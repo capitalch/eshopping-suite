382ca2654b0a038696c98165eaf05aefe01a99e7
capitalch <capitalch@gmail.com>
2018-02-04 21:14:23 +0530

    init

--- eshop/src/app/emart/emart.module.ts
+++ eshop/src/app/emart/emart.module.ts
@@ -27,13 +27,8 @@ import { CartUnitComponent } from './sub-components/cart-unit/cart-unit.componen
   declarations: [CompositeComponent, 
     ProductComponent, 
     ProductDetailsComponent,
-<<<<<<< HEAD
     HeaderComponent, CategoryComponent, FooterComponent, CartComponent, CartUnitComponent
   ],
   providers:[EmartService]
-=======
-    HeaderComponent, CategoryComponent, FooterComponent, CartComponent, FilterComponent
-  ]
->>>>>>> 6dfea9f71f51ae4894db0b9ea13f4bb4b6d6e589
 })
 export class EmartModule { }

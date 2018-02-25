e292bd5da773e2a951af843131b379d19918915d
capitalch <capitalch@gmail.com>
2018-02-25 21:04:38 +0530

    init

--- eshop-test2/src/app/app.module.ts
+++ eshop-test2/src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ReactiveFormsModule, FormsModule
   ],
   providers: [AppService],
   bootstrap: [AppComponent]

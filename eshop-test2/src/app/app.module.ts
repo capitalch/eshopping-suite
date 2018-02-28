741788ed54cf01a1328b27c4e8ff1603c0861bf0
SushantAgrawal <sagarwal@netwoven.com>
2018-02-28 13:58:14 +0530

    init

--- eshop-test2/src/app/app.module.ts
+++ eshop-test2/src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { Formio1Component } from './formio1/formio1.component';
 import { FormioModule } from 'angular-formio';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 // import {FormioGridModule} from 'angular-formio';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupFormComponent,
-    Formio1Component
+    Formio1Component,
+    ReactiveFormComponent
   ],
   imports: [
-    BrowserModule, ReactiveFormsModule, FormsModule, FormioModule
+    BrowserModule, ReactiveFormsModule,  FormioModule
   ],
   providers: [AppService],
   bootstrap: [AppComponent]

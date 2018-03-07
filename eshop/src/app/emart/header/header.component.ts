1fd4117c7cdeefc91a35f885bcb64e1bee33d1fc
Sudipto Mukherjee <sudiptokush@gmail.com>
2018-03-07 12:24:49 +0530

    changes

--- eshop/src/app/emart/header/header.component.ts
+++ eshop/src/app/emart/header/header.component.ts
@@ -16,6 +16,7 @@ import { Pop } from './populate-test-data';
 export class HeaderComponent implements OnInit {
   subs: any;
   searchString: string = '';
+  artifact = '';
   constructor(private emartService: EmartService, private brokerService: BrokerService, private appService: AppService) {
   }
 
@@ -23,15 +24,21 @@ export class HeaderComponent implements OnInit {
   }
 
   doSearch() {
-    let artifact;
     this.searchString ?
-      (artifact = httpMessages.searchProductsCategoriesOnCriteria)
-      : (artifact = httpMessages.categoriesWithCount);
-    this.brokerService.httpPost(httpMessages.headerToCategory, { id: artifact, params: [this.searchString, this.searchString] }, null, this.searchString);
+      (this.artifact = httpMessages.searchProductsCategoriesOnCriteria)
+      : (this.artifact = httpMessages.categoriesWithCount);
+    this.brokerService.httpPost(httpMessages.headerToCategory, { id: this.artifact, params: [this.searchString, this.searchString] }, null, this.searchString);
   }
 
   populate() {
     Pop.doPopulate();
+  }
+
+  clearSearchString()
+  {
+    this.searchString = "";
+    this. artifact = httpMessages.categoriesWithCount;
+    this.brokerService.httpPost(httpMessages.headerToCategory, { id: this.artifact, params: [this.searchString, this.searchString] }, null, this.searchString);
   }
 
   ngOnDestroy() {

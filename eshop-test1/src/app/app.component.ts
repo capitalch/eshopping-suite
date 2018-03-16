da8dc7f762969b23be0d0e0b9fea16cea0ef8ed6
SushantAgrawal <sagarwal@netwoven.com>
2018-03-16 11:04:59 +0530

    init

--- eshop-test1/src/app/app.component.ts
+++ eshop-test1/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Validators } from '@angular/forms';
+import { Validators, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,10 +9,140 @@ export class AppComponent {
   title = 'app';
   myLayout: any = {};
   content:string;
+ 
   ngOnInit() {
-    this.myLayout = {
-      firstName: ''
-    };
+    this.myLayout = [{
+      type: "text"
+      , id: "firstName"
+      , label: "First name"
+      , placeholder: "First name"
+      , value: ""
+      , validation: {
+        required: { message: '$ is required' },
+        minlength: { value: 3, message: 'Minimum length for $ is 3' },
+        maxlength: { value: 10, message: 'Maximum length for $ is 10' },
+        // ,email: true
+        myValidate: { value: this.myValidate('test'), message: 'My validation fails' }
+      }
+      , asyncValidation:{
+        async1:{
+          url:""
+          , value:""
+          , message:""
+        }
+      }
+    },
+    {
+      type: "text"
+      , id: "lasttName"
+      , label: "Last name"
+      , placeholder: "Last name"
+      , value: ""
+      , validation: {
+        required: { message: '$ is required' },
+        minlength: { value: 3, message: 'Minimum length for $ is 3' },
+        maxlength: { value: 10, message: 'Maximum length for $ is 10' }
+      }
+    },
+    {
+      type: "textarea"
+      , id: "address"
+      , label: "Address"
+      , placeholder: "Address"
+      , value: "12345"
+      , validation: {
+        required: { message: '$ is required' },
+        minlength: { value: 5, message: 'Minimum length for $ is 5' },
+        maxlength: { value: 200, message: 'Maximum length for $ is 200' }
+      }
+    },
+    {
+      type: "date"
+      , id: "date"
+      , label: "Date"
+      , placeholder: "Date"
+      , value: ""
+      , validation: {
+        required: { message: '$ is required' }
+      }
+    },
+    {
+      type: "checkbox"
+      , id: "agreed"
+      , label: "Agreed"
+      , value: false
+    }, {
+      type: "radio"
+      , label: "Gender"
+      , value: "M"
+      , id: "gender"
+      , options: [
+        { label: "Male", value: "M", id: "male" }
+        , { label: "Female", value: "F", id: "female" }
+      ]
+    }, {
+      type: "checkboxGroup"
+      , label: "Food1"
+      , id: "food1"
+      , validation: {
+        required: { message: '$ is required' }
+      }
+      , options: [
+        { label: "Main course", value: false, id: "main1" }
+        , { label: "Desert", value: true, id: "desert1" }
+        , { label: "beverages", value: false, id: "beverages1" }
+      ]
+    },
+    {
+      type: "select"
+      , label: "Country"
+      , value: "0"
+      , id: "country"
+      , options: [
+        { label: "---Choose---", value: "0" }
+        , { label: "USA", value: "us" }
+        , { label: "India", value: "in" }
+      ]
+      , validation: {
+        selectRequired: {
+          value: this.selectRequiredValidator('0')
+          , message: 'You must select a value for $'
+        }
+      }
+    },
+    {
+      type: "checkboxGroup"
+      , label: "Food"
+      , id: "food"
+      , validation: {
+        required: { message: '$ is required' }
+      }
+      , options: [
+        { label: "Main course", value: false, id: "main" }
+        , { label: "Desert", value: true, id: "desert" }
+        , { label: "beverages", value: false, id: "beverages" }
+      ]
+    },
+    {
+      type: 'submit'
+      , label: 'Submit'
+      , class:'btn btn-primary submit'
+      , submit: () => { console.log("submit clicked") }
+    }];
     this.content = "This is code"
+  }
+
+  myValidate(s) {
+    let func = (control: FormControl) => {
+      return (control.value.indexOf(s) >= 0 ? null : { myValidate: "true" });
+    };
+    return (func);
+  }
+
+  selectRequiredValidator(def) {
+    let func = (control: FormControl) => {
+      return ((control.value == def) ? { selectRequired: true } : null);
+    }
+    return (func);
   }
 }
--- eshop-test1/src/app/json-form/json-form.component.ts
+++ eshop-test1/src/app/json-form/json-form.component.ts
@@ -22,126 +22,7 @@ export class JsonFormComponent implements OnInit {
   ngOnInit() {
     console.log('init');
     this.ee = new EventEmitter();
-    this.layouts = [{
-      type: "text"
-      , id: "firstName"
-      , label: "First name"
-      , placeholder: "First name"
-      , value: ""
-      , validation: {
-        required: { message: '$ is required' },
-        minlength: { value: 3, message: 'Minimum length for $ is 3' },
-        maxlength: { value: 10, message: 'Maximum length for $ is 10' },
-        // ,email: true
-        myValidate: { value: this.myValidate('test'), message: 'My validation fails' }
-      }
-      , asyncValidation:{
-        async1:{
-          url:""
-          , value:""
-          , message:""
-        }
-      }
-    },
-    {
-      type: "text"
-      , id: "lasttName"
-      , label: "Last name"
-      , placeholder: "Last name"
-      , value: ""
-      , validation: {
-        required: { message: '$ is required' },
-        minlength: { value: 3, message: 'Minimum length for $ is 3' },
-        maxlength: { value: 10, message: 'Maximum length for $ is 10' }
-      }
-    },
-    {
-      type: "textarea"
-      , id: "address"
-      , label: "Address"
-      , placeholder: "Address"
-      , value: "12345"
-      , validation: {
-        required: { message: '$ is required' },
-        minlength: { value: 5, message: 'Minimum length for $ is 5' },
-        maxlength: { value: 200, message: 'Maximum length for $ is 200' }
-      }
-    },
-    {
-      type: "date"
-      , id: "date"
-      , label: "Date"
-      , placeholder: "Date"
-      , value: ""
-      , validation: {
-        required: { message: '$ is required' }
-      }
-    },
-    {
-      type: "checkbox"
-      , id: "agreed"
-      , label: "Agreed"
-      , value: false
-    }, {
-      type: "radio"
-      , label: "Gender"
-      , value: "M"
-      , id: "gender"
-      , options: [
-        { label: "Male", value: "M", id: "male" }
-        , { label: "Female", value: "F", id: "female" }
-      ]
-    }, {
-      type: "checkboxGroup"
-      , label: "Food1"
-      , id: "food1"
-      , validation: {
-        required: { message: '$ is required' }
-      }
-      , options: [
-        { label: "Main course", value: false, id: "main1" }
-        , { label: "Desert", value: true, id: "desert1" }
-        , { label: "beverages", value: false, id: "beverages1" }
-      ]
-    },
-    {
-      type: "select"
-      , label: "Country"
-      , value: "0"
-      , id: "country"
-      , options: [
-        { label: "---Choose---", value: "0" }
-        , { label: "USA", value: "us" }
-        , { label: "India", value: "in" }
-      ]
-      , validation: {
-        selectRequired: {
-          value: this.selectRequiredValidator('0')
-          , message: 'You must select a value for $'
-        }
-      }
-    },
-    {
-      type: "checkboxGroup"
-      , label: "Food"
-      , id: "food"
-      , validation: {
-        required: { message: '$ is required' }
-      }
-      , options: [
-        { label: "Main course", value: false, id: "main" }
-        , { label: "Desert", value: true, id: "desert" }
-        , { label: "beverages", value: false, id: "beverages" }
-      ]
-    },
-    {
-      type: 'submit'
-      , label: 'Submit'
-      , class:'btn btn-primary submit'
-      , submit: () => { console.log("submit clicked") }
-    }
-
-    ];
+    
     this.config = {
       submitClass: "btn btn-primary"
     };
@@ -171,38 +52,12 @@ export class JsonFormComponent implements OnInit {
 
   }
 
-  ngAfterViewInit() {
-    // let formControls = {};
-    // this.layouts.forEach(x => {
-    //   if (x.type == 'checkboxGroup' && x.options) {
-    //     let childControls = {};
-    //     x.options.forEach(y => {
-    //       childControls[y.id] = y.value;
-    //     });
-    //     formControls[x.id] = this.fb.group(childControls);
-    //     let sub = this.ee.filter((d) => {
-    //       return (d == 'checkboxGroup');
-    //     }).subscribe(f => {
-    //       let ctrl = this.myForm.controls[x.id];
-    //       ctrl.setValidators(this.checkboxGroupRequiredValidator);
-    //     });
-    //     this.subs ? this.subs.add(sub) : (this.subs = sub);
-    //   }
-    //   else {
-    //     let validators = this.getValidators(x);
-    //     formControls[x.id] = [x.value, validators];
-    //   }
-    // });
-    // this.myForm = this.fb.group(formControls);
-    // this.ee.emit('checkboxGroup');
-  }
-
-  selectRequiredValidator(def) {
-    let func = (control: FormControl) => {
-      return ((control.value == def) ? { selectRequired: true } : null);
-    }
-    return (func);
-  }
+  // selectRequiredValidator(def) {
+  //   let func = (control: FormControl) => {
+  //     return ((control.value == def) ? { selectRequired: true } : null);
+  //   }
+  //   return (func);
+  // }
 
   checkboxGroupRequiredValidator(ctrl: any) {
     let isValid = false, ret = null;
@@ -213,12 +68,12 @@ export class JsonFormComponent implements OnInit {
     return (ret);
   }
 
-  myValidate(s) {
-    let func = (control: FormControl) => {
-      return (control.value.indexOf(s) >= 0 ? null : { myValidate: "true" });
-    };
-    return (func);
-  }
+  // myValidate(s) {
+  //   let func = (control: FormControl) => {
+  //     return (control.value.indexOf(s) >= 0 ? null : { myValidate: "true" });
+  //   };
+  //   return (func);
+  // }
 
   getValidators(layout) {
     let validators = layout.validation && Object.keys(layout.validation).map(x => {

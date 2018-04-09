bba3f1ed1b982b134069e86a10a51526cb3a568d
SushantAgrawal <sagarwal@netwoven.com>
2018-04-09 10:06:02 +0530

    init

--- eshop-test1/src/app/jx-controls/core.components.ts
+++ eshop-test1/src/app/jx-controls/core.components.ts
@@ -45,8 +45,7 @@ export class JxCheckboxComponent {
       <legend>{{layout.label}}</legend>
       <ng-container *ngFor="let option of layout.options">
         <input type="radio" [id]="option.id+idx" [formControlName]="layout.id" [value]="option.value" [name]="layout.id">
-        <label [for]="option.id+idx">{{option.label}}</label>
-        
+        <label [for]="option.id+idx">{{option.label}}</label>        
       </ng-container>
       <jx-error [layout]="layout" [parent]="parent"></jx-error>
     </fieldset>`

39cc230366251c7bd2c70f2ab08634cb62f28d0a
SushantAgrawal <sagarwal@netwoven.com>
2018-04-13 11:06:16 +0530

    init

--- eshop-test1/src/app/jx-controls/mat.components.ts
+++ eshop-test1/src/app/jx-controls/mat.components.ts
@@ -165,12 +165,6 @@ export class JxMatInputComponent {
     }
 }
 
-// <mat-form-field>
-//   <input matInput [matDatepicker]="picker" placeholder="Choose a date">
-//   <mat-datepicker-toggle matSuffix [for]="picker"></mat-datepicker-toggle>
-//   <mat-datepicker #picker></mat-datepicker>
-// </mat-form-field>
-
 @Component({
     selector: 'jxmat-datepicker',
     template: `    

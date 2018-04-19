c55680a54331bd528358df16a8b271fc401c44db
SushantAgrawal <sagarwal@netwoven.com>
2018-04-20 00:46:40 +0530

    init

--- eshop-test1/src/app1/jx-service/options-bank.ts
+++
@@ -1,8 +0,0 @@
-
-let countries = [
-    { name: "---select a country---", value: 0 }
-    , { name: "India", value: "in" }
-    , { name: "USA", value: "us" }
-    , { name: "Japan", value: "jp" }
-]
-export { countries };

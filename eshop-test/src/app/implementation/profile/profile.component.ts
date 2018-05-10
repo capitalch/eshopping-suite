ed7d76db8258e0f1316243d0b1f7f262e7a9755f
Sudipto Mukherjee <sudiptokush@gmail.com>
2018-05-10 11:48:43 +0530

    changes

--- eshop-test/src/app/implementation/profile/profile.component.ts
+++ eshop-test/src/app/implementation/profile/profile.component.ts
@@ -6,6 +6,7 @@ import 'rxjs/add/observable/of';
 import { IbukiService } from '../../gx-dynamic-form/service/ibuki.service';
 import { GxCustomService } from '../../service/gx-custom.service';
 import { form1 } from '../form-json';
+import {tempJson} from '../template'
 
 @Component({
   selector: 'app-profile',
@@ -19,6 +20,8 @@ export class ProfileComponent implements OnInit {
   options: any = {};
   content: string;
   subs: any;
+  template: any;
+  jsonArray: any = [{type: 'meta', client: {title: 'Template Form', class: 'form-style-1'}, id: 'template'}];
   constructor(
     private ibukiService: IbukiService
     , private httpClient: HttpClient
@@ -28,17 +31,35 @@ export class ProfileComponent implements OnInit {
   }
 
   ngOnInit() {
-    // this.options = {
-    //   wrapperClass: "form-style-1"
-    // };
-    this.myLayout = form1;
+    this.template = tempJson;
+    this.processTemplate();
+    //this.myLayout = form1;
+    this.myLayout = this.jsonArray;
     this.content = 'This is code';
-
+    console.log(this.jsonArray);
   }
 
-  // ngAfterViewInit() {
-  //   this.brokerService.emit("userComponents", "test");
-  // }
+  processTemplate()
+  {
+    var grouping;
+    var controlArray=[];
+    for (var index in this.template) {
+      var groupObj = this.template[index];
+      for(let groupTitle in groupObj)
+      {
+        grouping = {type:'group',label: groupTitle ,id: groupTitle.replace(/ /g, "-")};
+        for (var prop of groupObj[groupTitle])
+        {
+          controlArray.push({type:'input',subtype:'text', id: prop.replace(/ /g, "-"), label: prop , value:' ', validation: {required: { message: '$ is required' }}});
+        }
+        grouping["controls"] = controlArray;
+      };
+      this.jsonArray.push(grouping);
+      grouping = null;
+      controlArray = [];
+    }
+  }
+
 
   ngOnDestroy() {
     this.subs.unsubscribe();
